129255bedfbdee77f6c75abbe35bf7efece54320
catherinism <catcreates13@gmail.com>
2019-12-08 14:56:41 -0500

    add moods and modify createStore

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,8 +1,18 @@
-import {combineReducers, createStore} from 'redux'
+import { combineReducers, createStore, applyMiddleware } from 'redux'
+import moods from './reducers/moodsReducer'
 import quotes from './reducers/quotesReducer'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-const rootReducer = combineReducers( {
+const rootReducer = combineReducers({
+    moods,
     quotes
 })
 
-export default createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+export default createStore(
+    rootReducer, 
+    composeWithDevTools(
+        applyMiddleware(thunk),
+        // other store enhancers if any
+        )
+)   
