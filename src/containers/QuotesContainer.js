b13b349a8ebd4c1e98b6dee53d4f4d96f44e064c
catherinism <catcreates13@gmail.com>
2019-12-08 14:56:07 -0500

    import fetchquotes and import/export connect

--- src/containers/QuotesContainer.js
+++ src/containers/QuotesContainer.js
@@ -1,22 +1,23 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { fetchQuotes } from '../redux/actions/quotesActions'
 
-export default class QuotesContainer extends Component {
+class QuotesContainer extends Component {
     state = {
         quotes: []
     }
     componentDidMount() {
-        fetch("http://localhost:3001/api/v1/quotes")
-            .then(res => res.json())
-            .then(quotes => this.setState({quotes}))
+       this.props.fetchQuotes()
     }
+    
     render() {
-        if (this.state.quotes.length === 0) {
+        if (this.props.quotes.length === 0) {
             return <h1>Loading quotes...</h1>
         }
         return (
             <div>
                 <ul>
-                    {this.state.quotes.map(quote => (
+                    {this.props.quotes.map(quote => (
                     <div key = {quote.id} >
                         <p>Quote: {quote.phrase}</p>
                         </div>
@@ -26,3 +27,13 @@ export default class QuotesContainer extends Component {
         )
     }
 }
+
+const mapStateToProps = state => {
+    return {
+        quotes: state.quotes
+    }
+}
+export default connect(
+    mapStateToProps, 
+    { fetchQuotes }
+    )(QuotesContainer)
