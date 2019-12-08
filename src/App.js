f29209256c3c7bbdb9327af81ad1f2f8358ecd09
catherinism <catcreates13@gmail.com>
2019-12-08 14:55:29 -0500

    import moods container

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Home from './components/Home'
-// import MoodsContainer from './containers/MoodsContainer'
+import MoodsContainer from './containers/MoodsContainer'
 import QuotesContainer from './containers/QuotesContainer'
 
 class App extends Component {
@@ -12,7 +12,7 @@ class App extends Component {
       <Router>
         <div className="App">
           <Route exact path="/" component={Home} />
-          {/* <Route path="/" component={MoodsContainer} /> */}
+          <Route path="/" component={MoodsContainer} />
           <Route path="/" component={QuotesContainer} />
         </div>
       </Router>
