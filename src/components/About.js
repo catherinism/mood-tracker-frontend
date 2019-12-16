e73437c94bf89fd605b3b3996b57768bc40a695a
catherinism <catcreates13@gmail.com>
2019-12-15 20:30:28 -0500

    add about page

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class About extends Component {
+    render() {
+        return (
+            <div className='main-page'>
+                <h2>Mood Check is an app that tracks your mood daily.</h2>
+            </div>
+        )
+    }
+}
